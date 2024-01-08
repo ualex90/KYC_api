3e1c6fc1c0da9c4745b9ef338867aac95786bcf4
u_alex90 <u_alex90@mail.ru>
2024-01-09 00:24:03 +1000

    Create user test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,36 +2,34 @@ from typing import Generator
 
 import pytest
 from fastapi.testclient import TestClient
+from tortoise.contrib.fastapi import register_tortoise
 from tortoise.contrib.test import finalizer, initializer
 
+# from main import app
 from main import create_app
 from src.config import settings
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", autouse=True)
 def client() -> Generator:
     app = create_app()
-    # Создание тестовой базы данных
+    # Создание тестовой базы
     initializer(
         db_url=settings.TEST_DB_URL,
-        modules=settings.APPS_MODELS
+        modules=settings.APPS_MODELS,
     )
-    with TestClient(app) as test_client:
-        yield test_client
-    # Удаление тестовой базы данных
+    # Подключение к тестовой базе
+    register_tortoise(
+        app,
+        db_url=settings.TEST_DB_URL,
+        modules={'models': settings.APPS_MODELS},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as c:
+        yield c
+    # Удаление тестовой базы
     finalizer()
-
-
-@pytest.fixture
-def admin_data() -> dict:
-    data = {
-        "email": "admin@test.com",
-        "password": "123qwe",
-        "last_name": "Admin Last Name",
-        "first_name": "Admin First Name",
-        "surname": "Admin Surname",
-    }
-    return data
 
 
 @pytest.fixture
@@ -41,7 +39,7 @@ def user1_data() -> dict:
         "password": "123qwe",
         "last_name": "Test 1 Last Name",
         "first_name": "Test 1 First Name",
-        "surname": "Test 1 Surname",
+        "surname": "Test 1 Surname"
     }
     return data
 
@@ -53,6 +51,18 @@ def user2_data() -> dict:
         "password": "123qwe",
         "last_name": "Test 2 Last Name",
         "first_name": "Test 2 First Name",
-        "surname": "Test 2 Surname",
+        "surname": "Test 2 Surname"
+    }
+    return data
+
+
+@pytest.fixture
+def admin_data() -> dict:
+    data = {
+        "email": "admin@test.com",
+        "password": "123qwe",
+        "last_name": "Admin Last Name",
+        "first_name": "Admin First Name",
+        "surname": "Admin Surname"
     }
     return data
--- tests/users/test_user.py
+++ tests/users/test_user.py
@@ -1,7 +1,11 @@
 from fastapi.testclient import TestClient
-from tortoise import Tortoise
 
 
-def test_register_user(client: TestClient):
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    assert True
+def test_register_user(client: TestClient, user1_data: dict):
+    response = client.post("/api/users/register", json=user1_data)
+    assert response.status_code == 200
+
+
+def test_register_user2(client: TestClient, user2_data: dict):
+    response = client.post("/api/users/register", json=user2_data)
+    assert response.status_code == 200
